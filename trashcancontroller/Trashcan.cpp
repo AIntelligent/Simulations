4d83699cabd1ef58ec68b59bd9b556f8ccfae9f9
Hakan Emre Kartal <54192616+AIntelligent@users.noreply.github.com>
2024-03-23 21:07:02 +0300

    Update Trashcan.cpp

--- trashcancontroller/Trashcan.cpp
+++ trashcancontroller/Trashcan.cpp
@@ -2,9 +2,9 @@
 // Trashcan.cpp
 //
 // Author:
-//       Hakan E. Kartal <hek@nula.com.tr>
+//       Hakan Emre Kartal <hek@nula.com.tr>
 //
-// Copyright (c) 2021 Hakan E. Kartal
+// Copyright (c) 2021 Hakan Emre Kartal
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
